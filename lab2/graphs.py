e3e75b85d466476d77e3f5b7fba74629c31604fa
Adina Hellström <adinahellstrom@hotmail.com>
2021-11-26 10:53:01 +0100

    Create graphs.py

---
+++ lab2/graphs.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov 26 10:18:45 2021
+
+@author: adinahellstrom
+"""
+
+
+
+class Graph:
+    adjlist = listahejhej
+    def __init__(self, edgelist):
+        pass
+    def __len__(self):
+        pass
+    def add_edge(self, a, b):
+        pass
+    def add_vertex(self, a):
+        pass
+    def edges(self):
+        pass
+    def get_vertex_value(self):
+        pass
+    def neighbours(self, v):
+        pass
+    def remove_edge(self, a,b):
+        pass
+    def set_vertex_value(self, v,x):
+        pass
+    def vertices(self):
+        pass
+    
+class WeighthedGraph:
+    def __init__(self):
+        pass
+    def get_weight(self, a, b):
+        pass
+    def set_weight(self, a, b, w):
+        pass
+    
+        
+# in graphs.py
+#dijkstra(graph, source, cost=lambda u,v: 1)
+#visualize(graph, view='dot', name='mygraph', nodecolors={}, engine='dot')
