019af078a21ff552be4ad58489b0fa83b141ca8d
Adina Hellström <adinahellstrom@hotmail.com>
2021-12-02 21:15:49 +0100

    Updates

--- lab2/trams.py
+++ lab2/trams.py
@@ -5,63 +5,111 @@ Created on Fri Nov 26 10:18:50 2021
 
 @author: adinahellstrom
 """
+from graphs import *
+import json
+import math
+import sys
+sys.path.append('../lab1/')
+import tramdata as td
 
 
-class TramNetwork:
+
+"Objects of your own class can be included in dictionaries and arrays "
+"like any other built-in data type in python. In this case, we want a"
+"dictionary of objects of class TramStop and a dictionary of objects of class TramLine"
+
+
+
+class TramNetwork(WeightedGraph):
     
     def __init__(self, lines, stops, times):
-        self.lines = lines
-        self.stops = stops
-        self.times = times
+        self._linedict = lines
+        self._stopdict = stops
+        self._timedict = times
         pass
     def all_lines(self):
-        pass
+        return list(self._linedict.keys())
     def all_stops(self):
-        pass
+        return list(self._stopdict.keys()) 
     def extreme_positions(self):
         pass
-    def geo_distance(self, a,b):
-        pass
+    def geo_distance(self, stops, a,b):
+        D = td.distance_between_stops(stops, a, b)
+        return D
     def line_stops(self, line):
-        pass
+        return self._linedict[line]
     def remove_lines(self, lines):
         pass
     def stop_lines(self, a):
         pass
     def transition_time(self, a,b):
         pass
-    
+
+
 class TramLine(TramNetwork):
     def __init__(self, num, stops):
-        self.num = num
-        self.stops = stops
+        self._num = num
+        self._stops = stops
     def get_number(self):
-        pass
+        return self._num
     def stops(self):
-        pass
-    
-class TramStops(TramLine):
+        return self._stops
+
+
+class TramStops(TramLine): #Kanske behövs arv från TramNetwork här också
     def __init__(self, name, lines, lat, lon):
-        self.name = name
-        self.lines = lines
-        self.lat = lat
-        self.lon = lon
+        self._name = name
+        self._lines = lines
+        self._pos = (lat, lon)
     def add_line(self, line):
-        pass
+        self._lines.append(line)
     def get_lines(self):
-        pass
+        return self._lines
     def get_name(self):
-        pass
+        return self._name
     def get_position(self):
-        pass
-    def set_position(self, lat,lon):
-        pass
+        return self._pos
+    def set_position(self, lat, lon):
+        self._pos = (lat, lon)
+
     
-def readTramNetwork(file=filename):
-    pass
-    
-# in trams.py
-readTramNetwork(file='tramnetwork.json')
+TRAM_FILE = 'tramnetwork.json'
         
+"It should return an object of class ``TramNetwork`."
+def readTramNetwork(tramfile=TRAM_FILE):
+    with open(TRAM_FILE) as f:
+        data = json.load(f)
+    linedict = data['lines']
+    stopdict = data['stops']
+    timedict = data['times']
+    
+    #print(linedict, stopdict, timedict)
+    
+    TramStops_dict = {}
+    
+    for stop in stopdict:
+        lines_via_stop_list = []
+        for line in linedict:
+            if stop in linedict[line]:
+                lines_via_stop_list.append(line)
+        TramStop_obj = TramStops(stop, lines_via_stop_list, stopdict[stop]['lat'], stopdict[stop]['lon'])
+        TramStops_dict[TramStop_obj.get_name()] = TramStop_obj.get_position()
+    
+    
+    TramLine_dict = {}
+    for line in linedict:
+        TramLine_obj = TramLine(line, linedict[line])
+        TramLine_dict[TramLine_obj.get_number()] = TramLine_obj.stops()
+    
+    
+    TramNetwork_obj = TramNetwork(TramLine_dict, TramStops_dict, timedict)
+    
+    print(TramLine_dict, TramStops_dict)
+    
+    return TramNetwork_obj
+    
+    
+readTramNetwork(TRAM_FILE)
+
 
         
